6457fe2d8cb5894fcd6716d0ef0d818733714260
mateki0 <mateki0@interia.pl>
2020-12-19 10:01:14 +0100

    path added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 
